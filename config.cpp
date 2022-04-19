0ee753b4afb5b40ccbaccbb78a6a163f9a8b5be9
simonvic <simonvic.dev@gmail.com>
2022-04-19 15:09:25 +0200

    Update config to new AWS version

--- config.cpp
+++ config.cpp
@@ -5,7 +5,17 @@ class CfgPatches {
 		requiredVersion = 0.1;
 		requiredAddons[] = {
 			"sGunplay",
-			"AdvancedWeaponScopes"
+			"AD_ACOG",
+			"AD_B13Mount",
+			"AD_DHF5",
+			"AD_LeupoldMk4",
+			"AD_NFATACR",
+			"AD_NFATACR_1_8",
+			"AD_Pilad",
+			"AD_PVS4",
+			"AD_RMR",
+			"AD_SpecterDR",
+			"AD_XPS34"
 		};
 	};
 };
@@ -24,54 +34,116 @@ class CfgMods {
 		version = "0.1";
 		extra = 0;
 		type = "mod";
-		
+
 		dependencies[] = {};
 	};
 };
 
 class cfgVehicles{
-	
+
 	class ItemOptics;
-	
+
+	// AD_ACOG /////////////////////////////////////////////////////////
+
 	class AD_ACOG_Base: ItemOptics {
 		class OpticsInfo {
 			PPMaskProperties[] = {0.0, 0.0, 0.33, 0.001};
-			PPLensProperties[] = {0.5, 0.0, 0.0, 0.0};
+			PPLensProperties[] = {0.5, 0.0, 0.0, 0.2};
 		};
 	};
-	
-	class AD_SpecterDR_Base : ItemOptics {
+
+	class AD_ACOG_RMR_Base: ItemOptics {
 		class OpticsInfo {
-			PPMaskProperties[] = {0.0, 0.0, 0.5, 0.001};
+			PPMaskProperties[] = {0.0, 0.0, 0.33, 0.001};
+			PPLensProperties[] = {0.5, 0.0, 0.0, 0.2};
+		};
+	};
+
+
+	// AD_DHF5 /////////////////////////////////////////////////////////
+	class AD_DHF5_Base : ItemOptics {
+		class OpticsInfo {
+			PPMaskProperties[] = {0.0, 0.0, 0.7, 0.001};
 			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
 		};
 	};
-	
+
+
+	// AD_LeupoldMk4 /////////////////////////////////////////////////////////
+	class AD_LeupoldMk4_Base : ItemOptics {
+		class OpticsInfo {
+			PPMaskProperties[] = {0.0, 0.0, 0.7, 0.001};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
+		};
+	};
+
+
+	// AD_NFATACR /////////////////////////////////////////////////////////
+	class AD_NFATACR_Base : ItemOptics {
+		class OpticsInfo {
+			PPMaskProperties[] = {0.0, 0.0, 0.7, 0.001};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
+		};
+	};
+
+
+	// AD_B13Mount /////////////////////////////////////////////////////////
+	class AD_NFATACR_B13_RMR : AD_NFATACR_Base {
+		class OpticsInfo {
+			PPMaskProperties[] = {0.0, 0.0, 0.7, 0.001};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
+		};
+	};
+
+
+	// AD_NFATACR_1_8 /////////////////////////////////////////////////////////
+	class AD_NFATACR_1_8_Base : ItemOptics {
+		class OpticsInfo {
+			PPMaskProperties[] = {0.0, 0.0, 2.0, 0.001};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
+		};
+	};
+
+
+	// AD_Pilad /////////////////////////////////////////////////////////
+	class AD_Pilad_Base : ItemOptics {
+		class OpticsInfo {
+			PPMaskProperties[] = {0.0, 0.0, 0.7, 0.001};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
+		};
+	};
+
+
+	// AD_PVS4 /////////////////////////////////////////////////////////
 	class AD_PVS4_Base : ItemOptics {
 		class OpticsInfo {
-			PPMaskProperties[] = {0.0, 0.0, 2.0, 0.05};
+			PPMaskProperties[] = {0.0, 0.0, 2.0, 0.001};
 			PPLensProperties[] = {-0.5, 0.0, 0.0, 0.0};
 		};
 	};
-	
-	class AD_Pilad_Base : ItemOptics {
+
+
+	// AD_SpecterDR /////////////////////////////////////////////////////////
+	class AD_SpecterDR_Base : ItemOptics {
 		class OpticsInfo {
-			PPMaskProperties[] = {0.0, 0.0, 2.0, 0.01};
-			PPLensProperties[] = {0.3, 0.0, 0.0, 0.2};
+			PPMaskProperties[] = {0.0, 0.0, 0.5, 0.0075};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
 		};
 	};
-	
-	class AD_NFATACR_1_8_Base : ItemOptics {
+
+
+	// AD_XPS34 /////////////////////////////////////////////////////////
+	class AD_XPS34_HSS_Base : ItemOptics {
 		class OpticsInfo {
-			PPMaskProperties[] = {0.0, 0.0, 2.0, 0.01};
-			PPLensProperties[] = {0.3, 0.0, 0.0, 0.2};
+			PPMaskProperties[] = {0.0, 0.02, 0.5, 0.0075};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
 		};
 	};
-	
-	class AD_NFATACR_Base : ItemOptics {
+
+	class AD_G33_Base : ItemOptics {
 		class OpticsInfo {
-			PPMaskProperties[] = {0.0, 0.0, 2.0, 0.01};
-			PPLensProperties[] = {0.3, 0.0, 0.0, 0.2};
+			PPMaskProperties[] = {0.0, 0.015, 0.4, 0.001};
+			PPLensProperties[] = {0.3, 0.0, 0.0, 0.3};
 		};
 	};
 };
